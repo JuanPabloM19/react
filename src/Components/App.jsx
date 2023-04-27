136a9b670714279492f01018d6eb4f7c475c02f2
Juan Pablo <jpmunizaga19@gmail.com>
2023-04-27 09:55:28 -0300

    cambios de importacion

---
+++ src/Components/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Checkout } from "./Checkout/Checkout";
+import { ItemDetailConatiner } from "./ItemDetailContainer/ItemDetailContainer";
+import { ItemListContainer } from "./ItemListContainer/ItemListContainer";
+import { NavBar } from "./NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <main className="app">
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/category/:category" element={<ItemListContainer />} />
+            <Route path="/product/:id" element={<ItemDetailConatiner />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import {App} from './App';
+import App from './Components/App';
 import { CarritoProvider } from './Context/CartContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
